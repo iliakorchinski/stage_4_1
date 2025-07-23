d768b099afc1cf2eef800fbb76ca3b7c07b97ae7
Ilia Kortchinski <kortchinskii@gmail.com>
2025-07-23 10:13:27 +0300

    feat: move initial state of authentication into separate constant and remove alert for handling error

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Form, useNavigate } from 'react-router-dom';
 import classes from './Login.module.css';
 import { login } from '../../store/authSlice';
@@ -6,17 +6,21 @@ import { useDispatch } from 'react-redux';
 import { User } from '../../data/user';
 
 export const Login = () => {
+  const initialState = { username: '', password: '', isError: false };
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [userData, setUserData] = useState({
-    username: '',
-    password: '',
-  });
+  const [userData, setUserData] = useState(initialState);
   const handleChangeField = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUserData((prevState) => {
       return { ...prevState, [event.target.name]: event.target.value };
     });
   };
+
+  useEffect(() => {
+    setUserData((prevState) => {
+      return { ...prevState, isError: false };
+    });
+  }, [userData.password, userData.username]);
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
@@ -27,7 +31,9 @@ export const Login = () => {
       dispatch(login());
       navigate('/');
     } else {
-      alert('Invalid credentials');
+      setUserData((prevState) => {
+        return { ...prevState, isError: true };
+      });
     }
   };
 
@@ -43,10 +49,15 @@ export const Login = () => {
           <label>Password:</label>
           <input type="password" name="password" onChange={handleChangeField} />
         </div>
+        {userData.isError && (
+          <p className={classes.error}>Invalid credentials</p>
+        )}
 
-        <button type="submit" className={classes.button}>
-          Login
-        </button>
+        {!userData.isError && (
+          <button type="submit" className={classes.button}>
+            Login
+          </button>
+        )}
       </Form>
     </div>
   );
